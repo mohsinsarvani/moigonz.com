4546d4614d171ac92172113a438011966f2550f9
Moises Gonzalez <moises.gonzalez@flixbus.com>
2019-08-06 15:38:03 +0200

    Adds initial content

---
+++ src/components/intro/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './intro.css';
+
+const Intro = () => (
+  <>
+    <div className="intro_container">
+      <h1>Hi, I&apos;m Moises Gonzalez and I do computer things</h1>
+    </div>
+  </>
+);
+
+export default Intro;
