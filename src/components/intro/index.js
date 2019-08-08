b8a66a34648a8eb6d58f7965b2ba322af335983b
Moises Gonzalez <moises.gonzalez@flixbus.com>
2019-08-08 22:39:37 +0200

    Adds link to github

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './intro.css';
 
+import Image from '../image';
+
 const Intro = () => (
   <>
+    <div className="image_container">
+      <Image />
+    </div>
     <div className="intro_container">
       <h1>
         Hi! I&apos;m Moises Gonzalez. I&apos;m based in
@@ -12,8 +17,16 @@ const Intro = () => (
         and I do computer stuff.
       </h1>
     </div>
-    <div />
-    <div />
+    <div className="social_container">
+      <a className="social_item" href="https://github.com/moigonzalez/">GitHub</a>
+      <div className="social_item">LinkedIn</div>
+      <div className="social_item">e-mail</div>
+      <div className="social_item">
+        Insta
+        <br />
+        gram
+      </div>
+    </div>
   </>
 );
 
