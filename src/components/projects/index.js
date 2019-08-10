20c67a8e2ef6f9b8b519b30fef9a2265fc29765c
Moises Gonzalez <moises.gonzalez@flixbus.com>
2019-08-10 21:18:57 +0200

    Finishes project section

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Grid from './grid';
+import Project from './project';
 
 import './projects.css';
 
@@ -12,16 +13,34 @@ const Projects = () => (
           <text x="1" y="15">PROJECTS</text>
         </svg>
       </div>
-      <div className="projects_item">
-        <div className="proj_title_wrapper"><h2 id="pwa_barcode_scanner" className="project_title">PWA Barcode Scanner</h2></div>
-        <div>
-          <div><a href="https://github.com/moigonzalez/pwa-barcode-scanner">GitHub</a></div>
-          <div><a href="https://pwascanit.com/">Live</a></div>
-        </div>
-      </div>
+      <Project
+        title={<h2 id="pwa_barcode_scanner" className="project_title">PWA Barcode Scanner</h2>}
+        links={[
+          {
+            text: 'GitHub',
+            link: 'https://github.com/moigonzalez/pwa-barcode-scanner',
+          },
+          {
+            text: 'Live',
+            link: 'https://pwascanit.com/',
+          },
+        ]}
+      />
       <div />
       <div />
-      <div />
+      <Project
+        title={<h2 id="parrotify_cli" className="project_title">parrotify-cli</h2>}
+        links={[
+          {
+            text: 'GitHub',
+            link: 'https://github.com/moigonzalez/parrotify-cli',
+          },
+          {
+            text: 'NPM',
+            link: 'https://www.npmjs.com/package/parrotify-cli',
+          },
+        ]}
+      />
     </Grid>
   </>
 );
