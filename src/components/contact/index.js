18fe1c1973befa6b71ea3107298d2faf120bdbe9
Moises Gonzalez <moises.gonzalez@flixbus.com>
2019-08-11 14:22:34 +0200

    Updates test

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -2,47 +2,45 @@ import React from 'react';
 
 import './contact.css';
 
-const Contact = () => (
-  <div className="contact_container">
-    <h1 className="contact_title">Contact me</h1>
-    <form name="contact" method="POST" data-netlify="true">
-      <p>
-        <label>
-          Your Name:
-          {' '}
-          <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Email:
-          {' '}
-          <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Role:
-          {' '}
-          <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select>
+const Contact = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
 
-        </label>
-      </p>
-      <p>
-        <label>
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: data,
+    })
+      .then(() => alert('Success!'))
+      .catch(error => alert(error));
+  };
+
+  return (
+    <div className="contact_container">
+      <h1 className="contact_title">Contact me</h1>
+      <form name="contact" method="POST" data-netlify="true" onSubmit={onSubmit}>
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>
+          Your Name:
+            {' '}
+            <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
           Message:
-          {' '}
-          <textarea name="message" />
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-  </div>
-);
+            {' '}
+            <textarea name="message" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
+  );
+};
 
 export default Contact;
