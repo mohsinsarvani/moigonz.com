36838b2bb8f99fb5d605c6b531d1b834f11212fd
Moises Gonzalez <moises.gonzalez@flixbus.com>
2019-08-12 17:00:16 +0200

    Finishes contact form

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -4,22 +4,17 @@ import encode from '../../helpers/encode';
 import './contact.css';
 
 const Contact = () => {
-  const [name, setName] = useState('');
-  // const [mail, setMail] = useState('');
+  const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
-
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(encode({
-      'form-name': 'contact', name, message,
-    }));
 
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
-        'form-name': 'contact', name, message,
+        'form-name': 'contact', email, message,
       }),
     })
       .then(() => alert('Success!'))
@@ -28,26 +23,27 @@ const Contact = () => {
 
   return (
     <div className="contact_container">
-      <h1 className="contact_title">Contact me</h1>
-      <form name="contact" action="/thanks/" method="POST" data-netlify="true" onSubmit={onSubmit}>
+      {/* eslint-disable-next-line jsx-a11y/no-distracting-elements */}
+      <marquee><h1 className="contact_title">Contact me</h1></marquee>
+      <form className="contact_form" name="contact" action="/" method="POST" data-netlify="true" onSubmit={onSubmit}>
         <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label>
-          Your Name:
-            {' '}
-            <input type="text" name="name" onChange={e => setName(e.target.value)} />
-          </label>
-        </p>
-        <p>
-          <label>
-          Message:
-            {' '}
-            <textarea name="message" onChange={e => setMessage(e.target.value)} />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+        <div className="contact_inputs_wrapper">
+          <div>
+            <label className="input_wrapper" htmlFor="email">
+              <span className="contact_label">E-mail</span>
+              <input className="contact_input" id="email" type="email" name="email" onChange={e => setEmail(e.target.value)} required palceholder="anexample@email.com" />
+            </label>
+          </div>
+          <div>
+            <label className="input_wrapper" htmlFor="message">
+              <span className="contact_label">Message</span>
+              <textarea className="contact_input" id="message" name="message" onChange={e => setMessage(e.target.value)} required placeholder="Hey there!" />
+            </label>
+          </div>
+        </div>
+        <div>
+          <button className="contact_button" type="submit">Send</button>
+        </div>
       </form>
     </div>
   );
