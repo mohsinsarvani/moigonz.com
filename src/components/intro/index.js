60e1592de032fc9fa1c22075eb429e7ebf136b43
Moises Gonzalez <moises.gonzalez@flixbus.com>
2019-08-17 10:50:44 +0200

    Fixes layout for firefox

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -6,36 +6,38 @@ import Grid from './grid';
 import Image from '../image';
 
 const Intro = () => (
-  <Grid>
-    <div className="image_container">
-      <Image />
-    </div>
-    <div className="intro_container">
-      {/* eslint-disable-next-line jsx-a11y/no-distracting-elements */}
-      <marquee className="intro_marquee">
-        <h1 className="contact_title">
-          <span role="img" aria-label="planet">️🌎</span>
-          {' '}
-            There&apos;s no planet B
-          {' '}
-          <span role="img" aria-label="fist">✊</span>
-        </h1>
-      </marquee>
-      <h1 className="title_intro">
-        Hi! I&apos;m Moises Gonzalez. I&apos;m based in
+  <>
+    {/* eslint-disable-next-line jsx-a11y/no-distracting-elements */}
+    <marquee className="intro_marquee">
+      <h1 className="contact_title">
+        <span role="img" aria-label="planet">️🌎</span>
         {' '}
-        <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Berlin">Berlin</a>
+          There&apos;s no planet B
         {' '}
-        and I do computer stuff.
+        <span role="img" aria-label="fist">✊</span>
       </h1>
-    </div>
-    <div className="social_container">
-      <a target="_blank" rel="noopener noreferrer" className="social_item" href="https://github.com/moigonzalez/">GitHub</a>
-      <a target="_blank" rel="noopener noreferrer" className="social_item" href="https://www.linkedin.com/in/moises-gonzalez-56969b78/">LinkedIn</a>
-      <a className="social_item" href="mailto:moigonzit@gmail.com">e-mail</a>
-      <a target="_blank" rel="noopener noreferrer" className="social_item" href="https://www.instagram.com/moi_gonz/">Instagram</a>
-    </div>
-  </Grid>
+    </marquee>
+    <Grid>
+      <div className="image_container">
+        <Image />
+      </div>
+      <div className="intro_container">
+        <h1 className="title_intro">
+          Hi! I&apos;m Moises Gonzalez. I&apos;m based in
+          {' '}
+          <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Berlin">Berlin</a>
+          {' '}
+          and I do computer stuff.
+        </h1>
+      </div>
+      <div className="social_container">
+        <a target="_blank" rel="noopener noreferrer" className="social_item" href="https://github.com/moigonzalez/">GitHub</a>
+        <a target="_blank" rel="noopener noreferrer" className="social_item" href="https://www.linkedin.com/in/moises-gonzalez-56969b78/">LinkedIn</a>
+        <a className="social_item" href="mailto:moigonzit@gmail.com">e-mail</a>
+        <a target="_blank" rel="noopener noreferrer" className="social_item" href="https://www.instagram.com/moi_gonz/">Instagram</a>
+      </div>
+    </Grid>
+  </>
 );
 
 export default Intro;
